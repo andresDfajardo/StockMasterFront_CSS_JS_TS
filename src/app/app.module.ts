8e81966aaf73aff9c6955751994bd4995501c511
luzangelagomezg <118863660+luzangelagomezg@users.noreply.github.com>
2023-10-30 10:33:18 -0500

    Se agrego el template html al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 // Librería para poder consumir el servicio
-import { HttpModule, } from '@angular/http';
 import { HttpClientModule, } from '@angular/common/http';
 import { RouterModule, Routes} from '@angular/router';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
@@ -36,7 +35,6 @@ const appRoutes: Routes =
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule,
     RouterModule.forRoot(appRoutes), // se agregan estos 
     HttpClientModule  // <- Agregar la clase    
   ],
